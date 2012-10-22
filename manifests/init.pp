60e246ddc159ab4a5b893a7922b0e6278d28c2dc
Cody Herriges <c.a.herriges@gmail.com>
2012-10-22 13:06:29 -0700

    Provide xinetd, ssl, normal service for bitlbee.
      Completed module provies xinetd support for bitlbee, as well as
      stunnel support to provide encryption.  There is also the stand linux
      service you can fall back on if need be.

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,7 +36,7 @@
 # Copyright 2011 Your name here, unless otherwise noted.
 #
 class bitlbee(
-  $run_mode       = 'Inetd',
+  $run_mode      = 'Inetd',
   $user          = 'bitlbee',
   $bind_address  = '0.0.0.0',
   $bind_port     = '6667',
@@ -57,8 +57,45 @@ class bitlbee(
     include xinetd
   }
 
+  # This is most important in configurations that use xinetd...we'll do it
+  # just because it doesn't hurt when not using xinetd.
+  augeas { 'bitlbee_services':
+    context => '/files/etc/services',
+    changes => [
+      'set service-name[last()+1] bitlbee',
+      "set service-name[. = 'bitlbee']/port ${bind_port}",
+      'set service-name[. = "bitlbee"]/protocol tcp',
+      'set service-name[. = "bitlbee"]#/comment "Bitlbee over SSL"',
+    ],
+    onlyif  => "match service-name[port = '${bind_port}'][protocol = 'tcp'] size == 0",
+    notify  => $run_mode ? {
+      'Inetd' => Xinetd::Service['bitlbee'],
+      default => Service['bitlbee'],
+    },
+  }
+  augeas { "bitlbee_services_${bind_port}":
+    context => '/files/etc/services',
+    changes => "set service-name[. = 'bitlbee']/port ${bind_port}",
+    onlyif  => "match service-name[port = '${bind_port}'][protocol = 'tcp'] size == 0",
+    require => Augeas['bitlbee_services'],
+    notify  => $run_mode ? {
+      'Inetd' => Xinetd::Service['bitlbee'],
+      default => Service['bitlbee'],
+    },
+  }
+
+  # How we manage a bitlbee server in different situations, xinetd vs. daemon.
   case $run_mode {
     'Inetd': {
+      if $ssl_cert {
+        file { '/etc/bitlbee/bitlbee.pem':
+          source => $ssl_cert,
+          mode   => '0600',
+          owner  => 'bitlbee',
+          group  => 'bitlbee',
+          before => Xinetd::Service['bitlbee'],
+        }
+      }
       xinetd::service { 'bitlbee':
         port         => $bind_port,
         server       => $ssl_cert ? {
@@ -66,16 +103,16 @@ class bitlbee(
           default => '/usr/bin/stunnel',
         },
         server_args  => $ssl_cert ? {
-          false   => undef
-          default => "-p ${ssl_cert} -l /usr/sbin/bitlbee",
+          false   => undef,
+          default => "-p /etc/bitlbee/bitlbee.pem -l /usr/sbin/bitlbee",
         },
         socket_type  => 'stream',
         protocol     => 'tcp',
-        user         => 'bittlbee',
+        user         => 'bitlbee',
         service_type => 'UNLISTED',
         require      => $ssl_cert ? {
           false   => File['bitlbee.conf'],
-          default => [ File[[ $ssl_cert, 'bitlbee.conf' ]], Class['stunnel'] ]
+          default => [ File['bitlbee.conf' ], Class['stunnel'] ],
         },
       }
     }
@@ -88,16 +125,35 @@ class bitlbee(
     }
   }
 
-
   package { 'bitlbee': ensure => present }
 
+  # The bitlbee config file, kind obvious.
   file { 'bitlbee.conf':
     path    => '/etc/bitlbee/bitlbee.conf',
-    content => template('bitlbee/bitlbee.conf'),
+    content => template('bitlbee/bitlbee.conf.erb'),
     mode    => '0640',
     owner   => 'root',
     group   => 'bitlbee',
     require => Package['bitlbee'],
   }
 
+  # Xinetd doesn't have a status command...yes this should be fixed in its
+  # module and not here.
+  Service <| title == 'xinetd' |> { hasstatus => false }
+
+  # This is completely unused...just that the stunnel service needs something
+  # to load or it never starts.
+  stunnel::tun { 'telnet':
+    certificate => "/var/lib/puppet/ssl/certs/${::clientcert}.pem",
+    private_key => "/var/lib/puppet/ssl/private_keys/${::clientcert}.pem",
+    ca_file     => '/var/lib/puppet/ssl/certs/ca.pem',
+    crl_file    => '/var/lib/puppet/ssl/crl.pem',
+    chroot      => '/var/lib/stunnel4/rsyncd',
+    user        => 'puppet',
+    group       => 'puppet',
+    client      => false,
+    accept      => '4423',
+    connect     => '23',
+    notify      => Service[$stunnel::data::service],
+  }
 }
